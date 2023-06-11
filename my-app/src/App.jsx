4d84ae0ba7b8fd8eac5b705e647ceb298bd2854c
mi3765 <chips1225@outlook.jp>
2023-06-11 22:16:39 +0900

    App.jsxを新規作成

---
+++ my-app/src/App.jsx
@@ -0,0 +1,54 @@
+import { React, useState } from "react";
+import "./App";
+
+export const App = () => {
+  const [todoText, setTodoText] = useState('');
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ああああ",
+    "いいいい",
+  ]);
+
+  const [completeTodos, setCompleteTodos] = useState([
+    "うううう",
+  ]);
+
+  const onChangeTodoText = (event) => {
+    setTodoText(event.target.value);
+  };
+
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText}/>
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
